ab7572c1fd8f52e0aa5655e2c10f041a33efb7a8
Gabriel <gabrieltggv29@gmail.com>
2020-07-04 07:34:35 -0300

    Consertado bug na configuração do heroku
    close #26

--- pypro/base/admin.py
+++ pypro/base/admin.py
@@ -43,7 +43,7 @@ class UserAdmin(admin.ModelAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
-    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('email', 'first_name', 'is_staff')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('first_name', 'email')
     ordering = ('first_name',)
