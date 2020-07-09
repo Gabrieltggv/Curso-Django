35deb333f317d15f23385e3fdec9c7653b9db7dc
Gabriel <gabrieltggv29@gmail.com>
2020-07-08 22:18:38 -0300

    Utilizada a template tag "static"
    close #31

--- pypro/settings.py
+++ pypro/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'base.User'
 
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 
 # Application definition
