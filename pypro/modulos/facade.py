b82d2d4f15107d7206a076c8a04d489f6ab475e8
Gabriel <gabrieltggv29@gmail.com>
2020-07-23 21:33:49 -0300

    Adicionado o processador de contexto
    close #58

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -3,7 +3,7 @@ from typing import List
 from pypro.modulos.models import Modulo
 
 
-def litar_modelos_ordenados() -> List[Modulo]:
+def listar_modulos_ordenados() -> List[Modulo]:
     """
     Lista módulos ordenados por títulos
     :return:
